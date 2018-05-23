d67377e46a0bc37f1d8b8954f9778bf15551a2e1
System Administrator <root@Ashiss-MacBook-Air.local>
2018-05-23 14:04:53 +0530

    New Design

---
+++ var.php.js
@@ -0,0 +1,17 @@
+//<![CDATA[
+var WEB_ROOT = '/';
+var WEB_ROOT_DATA = '/data/';
+var WEB_ROOT_COMPONENT = '/assets/';
+var WEB_ROOT_IMG = '/assets/img/';
+var WEB_ROOT_CSS = '/assets/css/';
+var WEB_ROOT_LESS = '/assets/less/';
+var WEB_ROOT_JS = '/assets/js/';
+var WEB_ROOT_LIB = '/assets/lib/';
+var WEB_ROOT_BUILD = '/assets/build/';
+var WEB_ROOT_VENDOR = '/vendor/';
+var WEB_ROOT_TMP = '/tmp/';
+var IP_CLIENT = '72.44.88.7';
+var HTTP = 'http://songes.jcsuzanne.com/';
+var HTTP_DATA = 'http://songes.jcsuzanne.com/data/';
+var MODE_TEMPLATE = '1';
+var MODE_COMPRESSION = '1';
